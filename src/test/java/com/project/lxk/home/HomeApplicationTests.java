dc04fbe43f1e02ac6d11618dd44a15b9a1934a6b
lixiaokang <xiaokang_5@126.com>
2023-05-27 14:47:07 +0800

    junit

--- src/test/java/com/project/lxk/home/HomeApplicationTests.java
+++ src/test/java/com/project/lxk/home/HomeApplicationTests.java
@@ -1,13 +1,24 @@
 package com.project.lxk.home;
 
+import com.project.lxk.home.dao.DemoDao;
+import com.project.lxk.home.dao.DemoReadDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HomeApplicationTests {
 
+  @Autowired
+  private DemoDao demoDao;
+
+  @Autowired
+  private DemoReadDao demoReadDao;
+
   @Test
   void contextLoads() {
+    System.out.println("M db count = " + demoDao.count());
+    System.out.println("R db count = " + demoReadDao.count());
   }
 
 }
